12a7408e1e0d3770d9d568262da016527bc98432
Dylan Peterson <101408728@student.swin.edu.au>
2018-09-20 15:49:08 +1000

    implementation

---
+++ ChallengeTESTWeb/ChallengeTESTWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChallengeTESTWeb.Startup))]
+namespace ChallengeTESTWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
